6efd78cf809f789c85da89e9e52da5d9cf97413a
timmyperrine <timmyperrine@gmail.com>
2021-02-11 08:20:27 -0500

    added account creation form and test php as an example of getting the user input via php

--- accountCreation.php
+++ accountCreation.php
@@ -4,6 +4,33 @@
 		<title>Account Creation</title>
 	</head>
 	<body>
+		<form class = "account-creation-form" action = "account-creation-functions.php" method="get">
+			<ul>
+				<li>
+					<label for="email-input">Email</label>
+					<input type="email" id = "email-input" name="email">
+				</li>
 
+				<li>					
+					<label for="password-input-input">Password</label>
+					<input type="password" id="password-input" name = "password" minlength="8" required
+					title = "Enter a password of at least 8 length">
+				</li>
+
+				<li>
+					<label for="f-name-input">First Name</label>
+					<input type="text" id="f-name-input" name = "f-name">
+				</li>
+
+				<li>
+					<label for="l-name-input">Last Name</label>
+					<input type="text" id="l-name-input" name = "l-name">
+				</li>
+
+				<li>
+					<input type="submit" id="submit-button" value="Create Account">
+				</li>
+			</ul>
+		</form>
 	</body>
 </html>
