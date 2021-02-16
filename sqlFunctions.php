f8a8fdaeb3b1cdf0070e6602b622f750d87336ee
timmyperrine <timmyperrine@gmail.com>
2021-02-16 18:41:27 -0500

    added a system where if the email that is being used for registration is already in use an error appears and the user is instructed to try again

--- sqlFunctions.php
+++ sqlFunctions.php
@@ -2,7 +2,16 @@
     -Figure out how $results data is stored and if it can be compared to string password.
     !-->
 
+<script>
+    function alertBox()
+        {
+            alert("Duplicate Email detected redirecting back to Account Creation");
+        }
+    </script>
+
 <?php
+
+        session_start();
 
 	function connectDB(){
 		$hn = "localhost";
@@ -21,9 +30,15 @@
         VALUES ('$email', '$user_name', '$passwrd');";
         $results = $connection->query($query);
         if(!$results){
-            echo "Query failed";
+            $_SESSION['register_success'] = false;
+            $_SESSION['registration_error'] = "Something went wrong with registration. Make sure you don't already have an account associated with this email.";
+            header("location:accountCreation.php");
             die("fatal error on sql query");
           } 
+    }
+
+    function sendToPage($page){
+        header("Location: $page");
     }
 
     //verify that given password matches account password
