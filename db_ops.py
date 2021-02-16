ea88ddb5e3ace36873c32e3b446f652dfc080bdd
pop-dev <pop-dev@pop-os.localdomain>
2021-02-15 20:18:55 -0500

    Added python script to handle the process of setting up and tearing down db environment

---
+++ db_ops.py
@@ -0,0 +1,41 @@
+# db_setup.py - Script that will set up all tables within 
+#               the ApartmentApplication database
+
+# NOTE:         This script assumes that python3 is installed and that the
+#               root account was used for the mysql install process
+
+import os
+import sys
+
+
+def call_script(script_dir, script_name):
+    script_path = os.path.join(script_dir, script_name)
+    os.system("mysql -u root -p < " + script_path)
+
+def print_usage():
+        print("Usaage: db_ops.py <setup/teardown>", file=sys.stderr)
+        sys.exit(1)
+
+def main():
+    SQL_SCRIPTS_DIR =   'sql_commands' 
+    SETUP_SCRIPT =      'db_account_setup.sql'
+    TEARDOWN_SCRIPT =   'db_account_teardown.sql'
+
+    if len(sys.argv) != 2:
+        print_usage()
+
+    if sys.argv[1] == 'setup':
+        print("[+] Setting up tables in ApartmentApplication database ... ")
+        call_script(SQL_SCRIPTS_DIR, SETUP_SCRIPT)
+        print("[+] Done")
+    
+    elif sys.argv[1] == 'teardown':
+        print("[+] Dropping tables in ApartmentApplication database ... ")
+        call_script(SQL_SCRIPTS_DIR, TEARDOWN_SCRIPT)
+        print("[+] Done")
+    
+    else:
+        print_usage()
+
+if __name__ == '__main__':
+    main()
