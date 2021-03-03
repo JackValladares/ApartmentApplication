7a1ffb2d988698fd35718751e12738f48aad461f
Jack Valladares <jackvalladares@gmail.com>
2021-03-03 17:17:48 -0500

    Fixed Toggle Visibility Function

--- login prototype/LoginFunctions.js
+++ login prototype/LoginFunctions.js
@@ -1,19 +1,21 @@
 
 var loginButton;
-var loginInterface;
+var loginInterface 
 var email;
 var pass;
 var confirmButton;
 
 
-	
-loginButton = document.getElementById("loginButton");
-confirmButton = document.getElementById("submitButton");
-loginInterface = document.getElementById("loginInterface");
-	
-loginButton.addEventListener("click", toggleVisibility, false);
-confirmButton.addEventListener("click", login, false);
+window.addEventListener('load', (event) => {	
 
+	loginButton = document.getElementById("loginButton");
+	confirmButton = document.getElementById("submitButton");
+	loginInterface = document.getElementById("loginInterface");;
+	
+	loginButton.addEventListener("click", toggleVisibility, false);
+	confirmButton.addEventListener("click", login, false);
+	console.log('Page is fully loaded');
+});
 
 
 
@@ -26,13 +28,17 @@ function toggleVisibility()
 	if(loginInterface.style.display === "none")
 	{
 		loginInterface.style.display = "block";
+		console.log('visible');
+		console.log(loginInterface.style.display)
 	} 
 	else
 	{
 		loginInterface.style.display = "none";
+		console.log('invisible');
+		console.log(loginInterface.style.display)
 	}
-	
-	this.removeEventListener("click", toggleVisibility);
+
+
 
 }
 
@@ -46,5 +52,6 @@ function login()
 	alert(email + " " + pass);
 	
 	this.removeEventListener("click", login);
+	console.log('Login Function Toggled Toggled');
 
 }
