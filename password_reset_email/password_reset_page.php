ddd2103f046a3778906e5039a0f3e92382f3b145
Harris <harriscollier108@gmail.com>
2021-03-09 15:55:36 -0500

    Adding change before updating repo

--- password_reset_email/password_reset_page.php
+++ password_reset_email/password_reset_page.php
@@ -4,16 +4,25 @@
     </head>
     <body>
         <?php
+            require_once 'sqlFunctions.php';
             if($_GET['key'] && $_GET['reset'])
             {
                 $email = $_GET['key'];
                 $password = $_GET['reset'];
                 //TODO: connect with sqli
+                $conn = connectDB();
                 $verify = "SELECT * FROM account WHERE md5(email) = $email AND md5(password) = $password";
-                if(mysqli_num_rows($verify) == 1)
+                
+                if(mysqli_num_rows($conn->query($verify) == 1)
                 {
                     ?>
-                    <form method="post" action=""
+                    <form method="post" action="submit_new.php">
+                    <input type="hidden" name="email" value="<?php echo $email; ?>">
+                    <p>Enter new password: </p>
+                    <input type="password" name='password'>
+                    <input type="submit" name="submit_password">
+                    </form>
+                    <?php
                 }
             }
         ?>
