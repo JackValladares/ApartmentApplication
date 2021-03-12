616c82ee956d8c62d142e8b2e0719773fca6fb8e
timmyperrine <timmyperrine@gmail.com>
2021-03-12 17:39:26 -0500

    something changed but i have no clue wht

---
+++ src/cssnhtml/account-creation-post-submit.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>TEST</title>
+	</head>
+	<body>
+		<?php
+			require_once("../php/sqlFunctions.php");
+
+			$conn = connectDB();
+			if(!$conn)
+			{
+				echo "database connection failure";
+				die("failed on DB connection");
+			}
+			$email = $_POST["email"];
+			$password = $_POST["password"];
+			$f_name = $_POST["f-name"];
+			$l_name = $_GET["l-name"];
+			$user_name = $f_name ." ". $l_name;
+
+			insertAccount($conn,$email,$user_name,$password);
+
+		  
+		?>
+	</body>
+</html>
