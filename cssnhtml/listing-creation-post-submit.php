9fe2e0dc1965927f505b2809d374142aec9f86fc
timmyperrine <timmyperrine@gmail.com>
2021-03-24 11:54:05 -0400

    user_id no work me sad

--- cssnhtml/listing-creation-post-submit.php
+++ cssnhtml/listing-creation-post-submit.php
@@ -20,23 +20,30 @@
 
 
     $conn = connectDB();
-    $email = $_SESSION['email'];
+    $email = $_GET['email'];
     $user_id = (int) get_user_id($conn, $email);
     
 
     /*
     Good Children:
-
+        Address
+        apt_no
+        city
+        state
+        room_size
+        bath_type
+        price
+        
 
 
     Problem Children:
-
+        user_id
     */
 
     $please = "INSERT INTO `Listing` (`address`, `apt_no`, `city`, `state`, `room_size`, `bath_type`, `price`, `user_id`) VALUES ('$address', '$apptNum', '$city', '$state', '$roomSize', '$bathType', '$price', '$user_id')"; 
-
+    $ahh = "INSERT INTO `Listing` (`user_id`) VALUES ('$user_id')";
     
-    insert_listing($conn, $please);
+    insert_listing($conn, $ahh);
     header("Location: gottem.php");
 
 ?>
